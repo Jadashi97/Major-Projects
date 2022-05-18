b5ed0319207af03872187d48bf742fc1840c2526
jadashi97 <njadauniversal23@yahoo.com>
2022-05-18 11:10:01 -0500

    (feat): add array to store the blog posts then log in the console. (fix): finish ch12

--- ejs-challenge/app.js
+++ ejs-challenge/app.js
@@ -4,6 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 
+let posts = [];
+
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
@@ -16,12 +18,13 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
+
 // this sends our content to the home root route
 app.get("/", function(req, res){
 
-
   res.render("home", {homeContent:homeStartingContent});
   
+  console.log(posts);
 });
 
 //shows the about page and it's content is rendered there
@@ -46,11 +49,22 @@ app.get("/compose", (req, res)=>{
 //create a post route to pass the infor that is publihed after hitting the compose button on the a new blog
 
 app.post('/compose' , (req , res)=>{
+  // this helps tap into the what user composes in the browser thru bodyparser
+  const theTitle = req.body.title;
+  const mainpost = req.body.maintext;
+ 
+  //create an object to store the infor passed by the user when they publish their written blog
+  const postedData = {
+    post : theTitle,
+    publish : mainpost,
+  };
+  posts.push(postedData);
 
+  // //turning the postedData above into a json format
+  // const jsonData = JSON.stringify(postedData)
 
-  const yourDay = req.body.publish // this helps tap into the what user composes in the browser thru bodyparser
-  //  res.send('hello from simple server :)')
-
+  //sends our user back to the root route
+  res.redirect("/")
 });
 
 
