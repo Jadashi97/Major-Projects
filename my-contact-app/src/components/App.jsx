9edd3ecdf4e1d5ac0a4069fb112e6fb7b652ced9
jadashi97 <njadauniversal23@yahoo.com>
2022-07-11 11:37:59 -0500

    add layout  and objects for app. (feat): create the card as a reusable component

--- my-contact-app/src/components/App.jsx
+++ my-contact-app/src/components/App.jsx
@@ -2,7 +2,21 @@ import React from "react";
 
 
 function App(){
-    return <h1>My contacts</h1>
+    return (
+    <div>
+        <h1 className="heading">My contacts</h1>
+        <div className="card">
+            <div className="top">
+                <h2>Lokose</h2>
+                <h2>Daniel</h2>
+            </div>
+            <div className="bottom">
+                <p>12345678</p>
+                <p>lokose@hotmail.com</p>
+            </div>
+        </div>
+    </div>
+    );
 }
 
 export default App;
---
+++ my-contact-app/src/contacts.js
@@ -0,0 +1,54 @@
+const contacts = [
+
+    {
+        firstName: "Nyarji",
+        lastName: "Jada",
+        otherNames: "kose",
+        birthDate: "7/4/1997",
+        userName: "jadashi97",
+        email: "kumbayas@hotmail.com",
+
+        address:{
+            country : "South Sudan",
+            City: "juba",
+            state: "CE",
+            Street: "123 gudele rd",
+        }
+
+
+    },
+    {
+        firstName: "Wani",
+        lastName: "Jada",
+        otherNames: "Ronald",
+        birthDate: "5/20/2000",
+        userName: "wycli",
+        email: "kumadss@hotmail.com",
+
+        address:{
+            country : "Uganda",
+            City: "kampala",
+            state: "NE",
+            Street: "13463 kampala rd",
+        }
+
+
+    },
+    {
+        firstName: "Toni",
+        lastName: "Roncales",
+        otherNames: "Rose",
+        birthDate: "2/15/1998",
+        userName: "bb1234",
+        email: "yaaasaayas@hotmail.com",
+
+        address:{
+            country : "USA",
+            City: "minneapolis",
+            state: "MN",
+            Street: "4000 franc rd",
+        }
+
+
+    },
+];
