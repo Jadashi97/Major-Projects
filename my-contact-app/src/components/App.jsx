de8aa7085af2e8c2ee1eaaa1843aacdf1ce719f0
jadashi97 <njadauniversal23@yahoo.com>
2022-07-14 12:03:30 -0500

    (fix): add needed components for the contacts app

--- my-contact-app/src/components/App.jsx
+++ my-contact-app/src/components/App.jsx
@@ -1,25 +1,16 @@
 import React from "react";
-import contacts from "../contacts";
-import { BrowserRouter } from "react-router-dom";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import Header from "./Header";
-import AddContact from "./AddContact";
+// import contacts from "../contacts";
+// import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+// import Header from "./Header";
+// import AddContact from "./AddContact";
 
 //this uses the card component created in card.jsx to dynamically create more contacts using the objects from contacts.js
 
 function App(){
-    return (
-      <Router>
-        <div className="fluid-container">
-          <div className="notification">
-          <Header style ={{marginBottom: "30px"}}/>
-          <h1 className="heading">My contacts</h1>
-          <Route exact path = "/" component = {Contacts}/>
-          <Route path = "/add" component = {AddContact}/>
-          </div>
-
-      </div>
-      </Router>
+    return (<h1>
+      My contacts
+    </h1>
     );
 }
 
