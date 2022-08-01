cd424b883f560b4ed24e177a434f8b1643201653
jadashi97 <njadauniversal23@yahoo.com>
2022-08-01 09:52:49 -0500

    (fix): start south sudanese church project

---
+++ southsudanese-church/src/App.js
@@ -0,0 +1,14 @@
+
+import React from "react";
+// import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>South Sudanese Community Church</h1>
+    </div>
+  );
+}
+
+export default App;
