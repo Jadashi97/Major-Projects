883409538e5634e22fee5d3d528b80882d2d7e3d
jadashi97 <njadauniversal23@yahoo.com>
2022-08-02 10:32:03 -0500

    (fix): add navbar to website. (feat):add features to header and design logo

--- southsudanese-church/src/components/App.jsx
+++ southsudanese-church/src/components/App.jsx
@@ -6,7 +6,6 @@ function App() {
   return (
     <div className="App">
       <Header/>
-      {/* <h1>South Sudanese Community Church</h1> */}
     </div>
   );
 }
--- southsudanese-church/src/components/Header.jsx
+++ southsudanese-church/src/components/Header.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 
+
+
+
 function Header() {
   return (
     <div>
+        <header>
+          <div class="topnav">
+            <h1>South Sudanese Church</h1>
+            <a href="#home">Home</a>
+            <a href="#news">News</a>
+            <a href="#contact">Contact</a>
+            <a href="#about" class="split">Help</a>
+          </div>
+        </header> 
         
     </div>
-    
+  
   );
 }
 
