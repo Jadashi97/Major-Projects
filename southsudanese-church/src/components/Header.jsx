ff7447029764698f7b1c3f4e04a8493bf1acf8c8
jadashi97 <njadauniversal23@yahoo.com>
2022-08-02 18:30:52 -0500

    (feat): restart ss church project. (fix):solve header problem

---
+++ southsudanese-church/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "./logo_ss2.jpg"
+
+function Header(){
+    return(
+        <div>
+            <img src={logo} alt="Logo" />;
+        </div>
+    )
+}
+
+export default Header;
