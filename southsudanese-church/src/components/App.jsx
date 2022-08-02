db81f1e3a5a95c77bb8f37c6f13a8172a467baff
jadashi97 <njadauniversal23@yahoo.com>
2022-08-01 20:50:57 -0500

    (fix): solve  boostrap issue. (feat):add features to header

--- southsudanese-church/src/components/App.jsx
+++ southsudanese-church/src/components/App.jsx
@@ -2,13 +2,11 @@
 import React from "react";
 import Header from "./Header";
 
-import "./index.css";
-
 function App() {
   return (
     <div className="App">
       <Header/>
-      <h1>South Sudanese Community Church</h1>
+      {/* <h1>South Sudanese Community Church</h1> */}
     </div>
   );
 }
