db5169c5afbde2001e53c9b139018d89235d3ac0
jadashi97 <njadauniversal23@yahoo.com>
2022-08-03 21:57:21 -0500

    (feat): add styles on header. (fix): add js for buttons to work

--- southsudanese-church/src/components/Header.jsx
+++ southsudanese-church/src/components/Header.jsx
@@ -2,11 +2,18 @@ import React from "react";
 function Header(){
     return(
         <div>
+            <div>
+                <img alt="myimage" src="/logo_ss1/jpg" width={20} height={20}></img>
+            </div>
             <div class="topnav">
                 <a class="active" href="#home">Home</a>
-                <a href="#news">News</a>
-                <a href="#contact">Contact</a>
                 <a href="#about">About</a>
+                <a href="#news">New Members</a>
+                <a href="#contact">Contact us</a>
+                <a href="#news">Sermons and Resources</a>
+                <a href="#news">Ministries</a>
+                <a href="#news"><button>Giving</button></a>
+
             </div>
             {/* <img src={logo} alt="Logo" />; */}
         </div>
