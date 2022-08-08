5414a2fec256906b05fa93bfb28ebd23e5b1d590
jadashi97 <njadauniversal23@yahoo.com>
2022-08-08 11:42:50 -0500

    (fix): add pics to the website

--- southsudanese-church/src/components/App.jsx
+++ southsudanese-church/src/components/App.jsx
@@ -2,12 +2,14 @@
 import Header from './Header';
 import Footer from './Footer';
 import BodyContent from './bodyContent';
+import CarouselFadeExample from "./bodyContent"
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <header className="App-header">
+      <CarouselFadeExample/>
       <BodyContent/>
         
       </header>
--- southsudanese-church/src/components/bodyContent.jsx
+++ southsudanese-church/src/components/bodyContent.jsx
@@ -1,12 +1,43 @@
-import React from "react";
+// import React from "react";
 
 
-function BodyContent(){
-    return(
-        <div className="picslides">
-            <img alt="" src={require("../images/souths.jpg")}/>
-        </div>    
-    )
+// function BodyContent(){
+//     return(
+//         <div className="picslides">
+//             <img alt="" src={require("../images/souths.jpg")}/>
+//         </div>    
+//     )
+// }
+
+// export default BodyContent;
+
+import Carousel from 'react-bootstrap/Carousel';
+
+function CarouselFadeExample() {
+  return (
+    <Carousel fade>
+      <Carousel.Item>
+        <img  className={("picslides")} alt="" src={require("../images/souths.jpg")}/>
+      </Carousel.Item>
+      <Carousel.Item>
+      <img  className={("picslides")} alt="" src={require("../images/souths.jpg")}/>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="holder.js/800x400?text=Third slide&bg=20232a"
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
 }
 
-export default BodyContent;
+export default CarouselFadeExample;
