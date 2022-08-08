dd0ab64cdf523f9df422017c95ae6bd6439c1a4e
jadashi97 <njadauniversal23@yahoo.com>
2022-08-08 07:14:03 -0500

    (feat): add table layout for the invoices

--- invoice-app/src/App.js
+++ invoice-app/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Table, Button} from "reactstrap";
 
 class App extends Component {
     state = { 
@@ -15,25 +16,51 @@ class App extends Component {
                 "id": "1",
                 "vendor": "Lokose",
                 "Amount": "$600",
+                "Invoice #": "123",
                 "Date": "1/10/22"
             },
             {
                 "id": "1",
                 "vendor": "wani",
                 "Amount": "$500",
+                "Invoice #": "123",
                 "Date": "1/10/22"
             },
             {
                 "id": "1",
                 "vendor": "Tom",
                 "Amount": "$400",
+                "Invoice #": "123",
                 "Date": "1/10/22"
             },
 
         ]
 
         return ( 
-            
+            <div className='container border border-secondary rounded center'>
+                <div className='row'>
+                    <div className='col-12'>
+                    <h4>Pending Invoices - The Test Company</h4>
+                    </div>
+                </div>
+
+                <div className='row'>
+                    <div className='.col-xs-12 center text-center'>
+                        <Table>
+                            <thead>
+                                <th>Vendor</th>
+                                <th>Amount</th>
+                                <th>Invoice #</th>
+                                <th>Date</th>
+                                <th>Actions</th>
+                            </thead>
+                        </Table>
+
+                    </div>
+
+                </div>
+
+            </div>
             );
     }
 }
