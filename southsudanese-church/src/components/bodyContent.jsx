6b582fe2725c5833f45085f80e94536e24f573c6
jadashi97 <njadauniversal23@yahoo.com>
2022-08-09 06:57:20 -0500

    (feat): add images slider for all my images

--- southsudanese-church/src/components/bodyContent.jsx
+++ southsudanese-church/src/components/bodyContent.jsx
@@ -1,76 +1,15 @@
 
-// import React, { useState } from 'react';
-// import Carousel from 'react-responsive-carousel/Carousel';
-
-// function ControlledCarousel() {
-//   const [index, setIndex] = useState(0);
-
-//   const handleSelect = (selectedIndex, e) => {
-//     setIndex(selectedIndex);
-//   };
-
-//   return (
-//     <Carousel activeIndex={index} onSelect={handleSelect}>
-//       <Carousel.Item>
-//         <img
-//           className="picslides"
-//           src={require("../images/ss6.jpg")}
-//           alt="First slide"
-//         />
-        // <img
-        //   className="d-block w-100"
-        //   src={require("../images/ss5.jpg")}
-        //   alt="Second slide"
-        // />
-//       </Carousel.Item>
-//       <Carousel.Item>
-//         {/* <img
-//           className="d-block w-100"
-//           src={require("../images/ss5.jpg")}
-//           alt="Second slide"
-//         /> */}
-//       </Carousel.Item>
-//       <Carousel.Item>
-        // <img
-        //   className="d-block w-100"
-        //   src={require("../images/ss2.jpg")}
-        //   alt="Third slide"
-        // />
-//       </Carousel.Item>
-//     </Carousel>
-//   );
-// }
-
-// export default ControlledCarousel;
-
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-const images = [
-        <img
-          className="picslides"
-          src={require("../images/ss6.jpg")}
-          alt="First slide"
-        />,
-        
-        <img
-          className="d-block w-100"
-          src={require("../images/ss5.jpg")}
-          alt="Second slide"
-        />,
-        <img
-          className="d-block w-100"
-          src={require("../images/ss2.jpg")}
-          alt="Third slide"
-        />, 
-];  
+import SliderData from "./sliderData";
 
 function ImageSlide() {
   return (
     <div className="box">
       <Carousel useKeyboardArrows={true}>
-        {images.map((URL, index) => (
+        {SliderData.map((sliderData, index) => (
           <div className="slide">
-            <img alt="sample_file" src={URL} key={index} />
+            <img alt="pic" src={SliderData.image} key={index} />
           </div>
         ))}
       </Carousel>
