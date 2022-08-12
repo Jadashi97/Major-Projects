f3b66ee43ce902779bf4f25b3107183225f5d183
jadashi97 <njadauniversal23@yahoo.com>
2022-08-11 20:04:42 -0500

    (fix): solve the carousel to display pics. (feat): add functionality features

--- southsudanese-church/src/components/App.jsx
+++ southsudanese-church/src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import Header from './Header';
 // import Footer from './Footer';
 import BodyContent from './bodyContent';
@@ -6,17 +6,12 @@ import Events from './Events';
 // import { Breakpoint, BreakpointProvider } from "react-socks";
 
 function App() {
-  const [width, setWindowWidth] = useState(0);
 
   return (
     <div className="App">
-      {/* <BreakpointProvider>
-      <Breakpoint></Breakpoint> */}
         <Header className="App-header"/>
         <BodyContent/>
         <Events/>
-      {/* <Footer/> */} 
-      {/* </BreakpointProvider> */}
     </div>
   );
 }
--- southsudanese-church/src/components/imageData.js
+++ southsudanese-church/src/components/imageData.js
@@ -1,46 +1,45 @@
 
 
 const items = [
+    // {
+    //     id:1,
+    //     image:<img href="#events" alt="myimage" src="/images/ss4.jpg" width={200} height={170}></img>
+
+    // },
+    // {
+    //     id:2,
+    //     image:<img href="#events" alt="myimage" src="/images/ss4.jpg" width={200} height={170}></img>
+    //     /Users/nyarjijada/Desktop/Major-projects/southsudanese-church/src/images/ss2.jpg
+
+    // },
+    // {
+    //     id:3,
+    //     image:<img href="#events" alt="myimage" src="/images/ss4.jpg" width={200} height={170}></img>
+    // },
     {
-        id:1,
-        image:<img href="#events" alt="myimage" src="./images/ss4.jpg"></img>
+        id:4,
+        image: require("../images/ss1.jpg")
 
     },
     {
-        id:2,
-        image:<img href="#events" alt="myimage" src="./images/ss4.jpg"></img>
-
+        id:5,
+        image: require("../images/ss5.jpg")
 
     },
     {
-        id:3,
-        image:<img href="#events" alt="myimage" src="./images/ss4.jpg"></img>
+        id:6,
+        image: require("../images/ss6.jpg")
 
     },
-    // {
-    //     id:4,
-    //     image: require("../images/ss4.jpg")
+    {
+        image: require("../images/ss3.jpg")
 
-    // },
-    // {
-    //     id:5,
-    //     image: require("../images/ss5.jpg")
+    },
+    {
+        id:7,
+        image: require("../images/ss11.jpg")
 
-    // },
-    // {
-    //     id:6,
-    //     image: require("../images/ss6.jpg")
-
-    // },
-    // {
-    //     image: require("../images/ss10.jpg")
-
-    // },
-    // {
-    //     id:7,
-    //     image: require("../images/ss11.jpg")
-
-    // },
+    },
 ]
 
 
