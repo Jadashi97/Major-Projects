ed50b3a0ff45979efc0b0ef80a56cb27ad21fa75
jadashi97 <njadauniversal23@yahoo.com>
2022-08-13 17:00:47 -0500

    (feat): add functionality to the nav bar links to go to the page wanted. (fix):solve slide show issue

--- southsudanese-church/src/components/bodyContent.jsx
+++ southsudanese-church/src/components/bodyContent.jsx
@@ -1,20 +1,74 @@
 
-import { Carousel } from "react-responsive-carousel";
+import React from "react";
+// import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import items from "./imageData"
+// import items from "./imageData";
+import { Slide } from "react-slideshow-image";
+
+
+// const items = [
+
+//   "/Users/nyarjijada/Desktop/Major-projects/southsudanese-church/src/images/ss1.jpg",
+
+//   "/images/ss1.jpg",
+
+//   "images/ss5.jpg",
+  
+//   "images/ss6.jpg",
+  
+//   "images/ss3.jpg",
+  
+//   "images/ss11.jpg"
+// ]
+
+const properties = {
+  duration: 5000,
+  transitionDuration: 500,
+  infinite: true,
+  indicators: true,
+  arrows: true,
+  onChange: (oldIndex, newIndex) => {
+    console.log(`slide transition from ${oldIndex} to ${newIndex}`);
+  }
+}
+
+const url = "src/images";
 
 function BodyContent() {
   return (
     <div>
       <h2>Let us come together to share and Worship with the Lord</h2>
-      <Carousel useKeyboardArrows={true}>
+      <div className="slide-container">
+        <Slide {...properties}>
+          <div className="each-slide">
+            <div style={{'backgroundImage': `url(${url})`}}>
+              <span>Slide 1</span>
+            </div>
+          </div>
+          <div className="each-slide">
+            <div style={{'backgroundImage': `url(${url})`}}>
+              <span>Slide 2</span>
+            </div>
+          </div>
+          <div className="each-slide">
+            <div style={{'backgroundImage': `url(${url})`}}>
+              <span>Slide 3</span>
+            </div>
+          </div>
+        </Slide>
+      </div>
+
+
+
+    {/* <Carousel useKeyboardArrows={true}>
         {items.map((image, index) => (
           <div >
             <img src={image} key ={index} alt={"pics"}/>
           </div>
         ))}
-      </Carousel>
+      </Carousel> */}
     </div>
   );
 }
 export default BodyContent;
+
--- southsudanese-church/src/components/imageData.js
+++ southsudanese-church/src/components/imageData.js
@@ -1,46 +1,18 @@
 
 
-const items = [
-    // {
-    //     id:1,
-    //     image:<img href="#events" alt="myimage" src="/images/ss4.jpg" width={200} height={170}></img>
+// const items = [
 
-    // },
-    // {
-    //     id:2,
-    //     image:<img href="#events" alt="myimage" src="/images/ss4.jpg" width={200} height={170}></img>
+//     "./images/ss1.jpg",
 
-    // },
-    // {
-    //     id:3,
-    //     image:<img href="#events" alt="myimage" src="/images/ss4.jpg" width={200} height={170}></img>
-    // },
-    // {
-    //     id:4,
-    //     image: require("./images/ss1.jpg")
-
-    // },
-    {
-        id:5,
-        image: require("../images/ss5.jpg")
-
-    },
-    {
-        id:6,
-        image: require("../images/ss6.jpg")
-
-    },
-    {
-        image: require("../images/ss3.jpg")
-
-    },
-    {
-        id:7,
-        image: require("../images/ss11.jpg")
-
-    },
-]
+//     "./images/ss5.jpg",
+    
+//     "./images/ss6.jpg",
+    
+//     "./images/ss3.jpg",
+    
+//     "./images/ss11.jpg"
+// ]
 
 
 
-export default items;
+// export default items;
