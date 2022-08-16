dfd98f5441befa68fadfb636164cd516c0e1d994
jadashi97 <njadauniversal23@yahoo.com>
2022-08-16 11:13:12 -0500

    (feat): added features for expenses.

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -1,15 +1,19 @@
 
 import './App.css';
-import Budget from './components/Budget';
+import Revenue from './components/Revenue';
+import Expenses from './components/Expenses';
 import Header from './components/Header';
+import Remaining from './components/Remaining';
 
 function App() {
   return (
     <div>
-      <div>
       <Header/>
+      <div className='budget'>
+        <Revenue/>
+        <Expenses/>
+        <Remaining/>  
       </div>
-      <Budget/>
     </div>
   );
 }
