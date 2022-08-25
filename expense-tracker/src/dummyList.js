6872ae507843210a51f0ee1c8444a27ea64fb2ef
jadashi97 <njadauniversal23@yahoo.com>
2022-08-25 10:27:50 -0500

    (feat): added edit starting code. (fix): solve the edit puzzle

---
+++ expense-tracker/src/dummyList.js
@@ -0,0 +1,20 @@
+const dummyList= [
+    {
+        id: 1,
+        name:"Rent",
+        cost: 500,
+    },
+    {
+        id: 2,
+        name:"Grocery",
+        cost: 150,
+    },
+    {
+        id: 3,
+        name:"car",
+        cost: 100,
+    }
+]
+
+
+export default dummyList;
